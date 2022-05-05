7c79a15686e15134ad129b8d1bca5f368dca1acd
dieselleo <diesel.leonardo@gmail.com>
2022-05-05 14:39:02 +0100

    FileExplorer added
    I added file explorer and made it work inside the application, but still, functionalities are not working well

---
+++ fileExplorer/search.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var lunr = require('lunr');
+var index;
+
+function resetIndex() {
+	index = lunr(function() {
+		this.field('file');
+		this.field('type');
+		this.ref('path');
+	});
+}
+
+function addToIndex(file) {
+	console.log(index)
+	console.log(file)
+	index.add(file);
+}
+
+function find(query, cb) {
+	if (!index) { resetIndex(); }
+
+	var results = index.search(query);
+	cb(results);
+}
+
+module.exports = {
+	addToIndex: addToIndex,
+	find: find,
+	resetIndex: resetIndex
+};
